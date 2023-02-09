afcdfc0ad4adafb3e3ab9e1e377ea159a0eeab59
cdq73700 <cdq73700@gmail.com>
2023-02-09 14:48:47 +0900

    uuid 追加

--- backend/src/users/migration.rs
+++ backend/src/users/migration.rs
@@ -1,12 +1,13 @@
 use backend::establish_connection;
 use diesel::prelude::*;
+use uuid::Uuid;
 
 use super::create::create_users;
 
 pub fn migration_users() {
     let connection: &mut MysqlConnection = &mut establish_connection();
 
-    let id: &str = "123456789";
+    let id: &str = &Uuid::new_v4().to_string();
     let name: &str = "test";
 
     let _users: super::model::Users = create_users(connection, id, name);
